683992296ccd7959d8753aebfc7b95404ddeef93
michaelbrinkworth <michaelhireitem@gmail.com>
2023-03-13 14:00:17 +1000

    push

--- pages/[uri].js
+++ pages/[uri].js
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Footer from '../components/Footer'
-import { getPostByUri } from '../lib/test-data';
+import Head from "next/head";
+import Footer from "../components/Footer";
+import { getPostByUri } from "../lib/test-data";
+import { client } from "../lib/apollo";
+import { gql } from "@apollo/client";
 
 export default function SlugPage({ post }) {
-
   return (
     <div>
       <Head>
@@ -12,37 +13,52 @@ export default function SlugPage({ post }) {
       </Head>
 
       <main>
-          <div className="siteHeader">
-            <h1 className="title">
-                {post.title}
-            </h1>
-            <p>✍️  &nbsp;&nbsp;{`${post.author.node.firstName} ${post.author.node.lastName}`} | 🗓️ &nbsp;&nbsp;{ new Date(post.date).toLocaleDateString() }</p>
-          </div>
-            <article dangerouslySetInnerHTML={{__html: post.content}}>   
-            </article>
+        <div className="siteHeader">
+          <h1 className="title">{post.title}</h1>
+          <p>
+            ✍️ &nbsp;&nbsp;
+            {`${post.author.node.firstName} ${post.author.node.lastName}`} | 🗓️
+            &nbsp;&nbsp;{new Date(post.date).toLocaleDateString()}
+          </p>
+        </div>
+        <article dangerouslySetInnerHTML={{ __html: post.content }}></article>
       </main>
 
       <Footer></Footer>
-
     </div>
-  )
+  );
 }
 
+export async function getStaticProps({ params }) {
+  const GET_LISTING_BY_URI = gql`
+    query GetListingByURI($id: ID!) {
+      listing(id: $id, idType: URI) {
+        title
+        uri
+        date
+      }
+    }
+  `;
 
-export async function getStaticProps({ params }){
-  const response = await getPostByUri(params.uri)
-  const post = response?.data?.post
+  const response = await client.query({
+    query: GET_LISTING_BY_URI,
+    variables: {
+      id: params.uri,
+    },
+  });
+  const post = response?.data?.listing;
   return {
     props: {
-      post
-    }
-  }
+      post,
+    },
+  };
 }
 
-export async function getStaticPaths(){
-    const paths = []
-    return {
-        paths,
-        fallback: 'blocking'
-    }
+//allows next js to pre render different paths and optimise app for those routes. pass in an empty array.
+export async function getStaticPaths() {
+  const paths = [];
+  return {
+    paths,
+    fallback: "blocking",
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import Head from 'next/head';
-import Footer from '../components/Footer';
-import PostCard from '../components/PostCard';
-import { getAllPosts } from '../lib/test-data';
-
+import Head from "next/head";
+import Footer from "../components/Footer";
+import PostCard from "../components/PostCard";
+import { getAllPosts } from "../lib/test-data";
+import { client } from "../lib/apollo";
+import { gql } from "@apollo/client";
 
 export default function Home({ posts }) {
   return (
@@ -13,37 +14,45 @@ export default function Home({ posts }) {
       </Head>
 
       <main>
-        <h1 className="title">
-          Headless WordPress Next.js Starter
-        </h1>
+        <h1 className="title">Hypley</h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
 
         <div className="grid">
-          {
-            posts.map((post) => {
-              return (
-                <PostCard key={post.uri} post={post}></PostCard>
-              )
-            })
-          }
+          {posts.map((post) => {
+            return <PostCard key={post.uri} post={post}></PostCard>;
+          })}
         </div>
       </main>
 
       <Footer></Footer>
     </div>
-  )
+  );
 }
 
-export async function getStaticProps(){
-
-  const response = await getAllPosts()
-  const posts = response?.data?.posts?.nodes
+//pass data into posts component in function above.
+export async function getStaticProps() {
+  const GET_LISTINGS = gql`
+    query GetAllListings {
+      listings {
+        nodes {
+          title
+          content
+          uri
+          date
+        }
+      }
+    }
+  `;
+  const response = await client.query({
+    query: GET_LISTINGS,
+  });
+  const posts = response?.data?.listings?.nodes;
   return {
     props: {
-      posts
-    }
-  }
+      posts,
+    },
+  };
 }
